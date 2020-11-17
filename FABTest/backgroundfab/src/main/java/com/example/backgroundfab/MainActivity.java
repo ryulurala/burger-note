ae012811b92fca74585edfa303c3a447a751252e
ryulurala <ryulurala@gmail.com>
2020-11-18 01:22:46 +0900

    Implements Convert secondView(Dialog)

--- FABTest/backgroundfab/src/main/java/com/example/backgroundfab/MainActivity.java
+++ FABTest/backgroundfab/src/main/java/com/example/backgroundfab/MainActivity.java
@@ -13,7 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1;
+    private static final int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1;             // for. 요청 받고 나온 값 검사
 
     @Override
     protected void onDestroy() {
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         bt_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkPermission();
+                checkPermission();          // 권한 검사 메소드
                 //startService(new Intent(MainActivity.this, AlwaysOnTopService.class));
             }
         });
@@ -48,21 +48,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void checkPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   // 마시멜로우 이상일 경우
-            if (!Settings.canDrawOverlays(this)) {              // 체크
-                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                        Uri.parse("package:" + getPackageName()));
-                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE);
-            } else {
-                startService(new Intent(MainActivity.this, MyService.class));
-            }
-        } else {
-            startService(new Intent(MainActivity.this, MyService.class));
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
+    @TargetApi(Build.VERSION_CODES.M)       // M버전 이상인 경우에만 적용 메소드
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -70,9 +56,25 @@ public class MainActivity extends AppCompatActivity {
             if (!Settings.canDrawOverlays(this)) {
                 // TODO 동의를 얻지 못했을 경우의 처리
 
-            } else {
+            } else {        // 권한 동의를 얻음
                 startService(new Intent(MainActivity.this, MyService.class));
             }
+        }
+    }
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   // 마시멜로우 이상일 경우
+            if (!Settings.canDrawOverlays(this)) {          // 권한을 아직 안받았다.
+                // 권한 setting 창을 키는 Intent
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                // 권한 Setting 창을 켬, 결과를 받는 메소드(+ Result)
+                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE);
+            } else {         // 이미 권한을 받았다.
+                startService(new Intent(MainActivity.this, MyService.class));
+            }
+        } else {        // 마시멜로우 미만 버전은 이미 설치했을 경우 권한 체크를 했다.
+            startService(new Intent(MainActivity.this, MyService.class));
         }
     }
 }
--- FABTest/backgroundfab/src/main/java/com/example/backgroundfab/MyService.java
+++ FABTest/backgroundfab/src/main/java/com/example/backgroundfab/MyService.java
@@ -7,24 +7,34 @@ import android.graphics.PixelFormat;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MyService extends Service {
 
-    WindowManager wm;
-    View mView;
-    WindowManager.LayoutParams mParams;
+    WindowManager wm;           // 윈도우 매니저(가장 바깥 쪽 View?)
+    View fstView;             // 오버레이할 View
+    View scdView;
+    Animation anim;
+    WindowManager.LayoutParams mParams;             // Layout 미리 속성들 지정
 
-    private float START_X, START_Y;							//움직이기 위해 터치한 시작 점
-    private int PREV_X, PREV_Y;								//움직이기 이전에 뷰가 위치한 점
-    private int MAX_X = -1, MAX_Y = -1;					//뷰의 위치 최대 값
+    private float START_X, START_Y;					// 움직이기 위해 터치한 시작 점
+    private int PREV_X, PREV_Y;						// 움직이기 이전에 뷰가 위치한 점
+    private int MAX_X = -1, MAX_Y = -1;				// 뷰의 위치 최대 값
+
+    private final int duration = 100;          // 터치 지속 시간
+    private final int buttonScale = 80;         // dp
+    private final int dialogScale = 200;        // dp
+    long initTime;                          // 터치시간
 
     @Override
     public IBinder onBind(Intent intent) { return null; }
@@ -32,27 +42,96 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        LayoutInflater inflate = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        wm = (WindowManager) getSystemService(WINDOW_SERVICE);
 
-        initWindowParams();
+        // 레이아웃 인플레이터를 가져옴
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        wm = (WindowManager) getSystemService(WINDOW_SERVICE);       // 윈도우 서비스의 Manager 가져옴
 
-        mView = inflate.inflate(R.layout.view_in_service, null);
+        initWindowParams();     // 레이아웃 속성들 초기화
 
-        mView.setOnTouchListener(new mViewTouchListener());
+        // 레이아웃을 객체로 만듬(Inflate)
+        fstView = layoutInflater.inflate(R.layout.first, null);
+        scdView = layoutInflater.inflate(R.layout.second, null);     // 나중에는 두 번째 파라미터로 view group 에 붙이기
 
-        final ImageButton bt =  (ImageButton) mView.findViewById(R.id.bt);
-        bt.setOnClickListener(new View.OnClickListener() {
+        // 애니메이션 불가 - 시스템 권한때문에, windowManager 를 move 하는 것처럼 update 시켜줘야 함.(timeout, tick)
+//        anim = AnimationUtils.loadAnimation(this, R.anim.scale_up);
+//        anim.setFillAfter(true);
+
+        final ImageButton bt = (ImageButton) fstView.findViewById(R.id.fstbt);
+        bt.setOnTouchListener(new View.OnTouchListener(){
             @Override
-            public void onClick(View v) {
-                bt.setImageResource(R.mipmap.ic_launcher_round);
-            }
-        });
+            public boolean onTouch(View view, MotionEvent motionEvent) {
 
-        wm.addView(mView, mParams);
+                switch(motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:                // 사용자 터치 다운이면(0)
+                        initTime = System.currentTimeMillis();          // 처음 터치 시간
+                        if(MAX_X == -1)
+                            setMaxPosition();
+                        START_X = motionEvent.getRawX();                    //터치 시작 점
+                        START_Y = motionEvent.getRawY();                    //터치 시작 점
+                        PREV_X = mParams.x;                            //뷰의 시작 점
+                        PREV_Y = mParams.y;                            //뷰의 시작 점
+                        break;
+                    case MotionEvent.ACTION_MOVE:                // 사용자 터치 무브면(2)
+                        int x = (int)(motionEvent.getRawX() - START_X);    //이동한 거리
+                        int y = (int)(motionEvent.getRawY() - START_Y);    //이동한 거리
+
+                        //터치해서 이동한 만큼 이동 시킨다
+                        mParams.x = PREV_X + x;
+                        mParams.y = PREV_Y + y;
+
+                        optimizePosition();        //뷰의 위치 최적화
+                        wm.updateViewLayout(fstView, mParams);    //뷰 업데이트
+                        break;
+                    // ----여기다가 시간에 따라서 onClick 처럼 보이게 작용--- 코드 작성
+                    case MotionEvent.ACTION_UP:
+                        Log.d("myLog", "initTime = " + initTime);
+                        if(System.currentTimeMillis() - initTime < duration){
+                            // onClick
+                            initDialogParams();          // layoutParams 속성 바꾸기(gravity 중점적으로)
+                            wm.addView(scdView, mParams);
+
+//                       scdView.startAnimation(anim);        // 시스템 권한은 애니메이션 불가
+
+                            wm.removeView(fstView);
+                            fstView = null;
+                        }
+                        break;
+                }
+                return true;            // Motion 이벤트 소모
+            }
+        });            // for. 움직임
+        wm.addView(fstView, mParams);
     }
 
     private void initWindowParams() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            mParams = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.WRAP_CONTENT,        // 변경해야함 - 부모의 가중치로 크기를 지정
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                            | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                            | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
+                            | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR,
+                    PixelFormat.TRANSLUCENT);
+        } else {
+            mParams = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                            | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                            | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
+                            | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR,
+                    PixelFormat.TRANSLUCENT);
+        }
+        mParams.width = buttonScale * (int)getResources().getDisplayMetrics().density;       // 80dp(나중에 수정 가능하도록, static??)
+        mParams.height = buttonScale * (int)getResources().getDisplayMetrics().density;      // 80dp
+        mParams.gravity = Gravity.LEFT | Gravity.TOP;
+    }
+
+    private void initDialogParams() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
             mParams = new WindowManager.LayoutParams(
                     WindowManager.LayoutParams.WRAP_CONTENT,
@@ -74,43 +153,16 @@ public class MyService extends Service {
                             | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR,
                     PixelFormat.TRANSLUCENT);
         }
-        mParams.gravity = Gravity.LEFT | Gravity.TOP;
+        mParams.width = dialogScale * (int)getResources().getDisplayMetrics().density;       // 80dp(나중에 수정 가능하도록, static??)
+        mParams.height = dialogScale * (int)getResources().getDisplayMetrics().density;      // 80dp
     }
-
-   class mViewTouchListener implements View.OnTouchListener {
-       @Override
-       public boolean onTouch(View view, MotionEvent motionEvent) {
-           switch(motionEvent.getAction()) {
-               case MotionEvent.ACTION_DOWN:                //사용자 터치 다운이면
-                   if(MAX_X == -1)
-                       setMaxPosition();
-                   START_X = motionEvent.getRawX();                    //터치 시작 점
-                   START_Y = motionEvent.getRawY();                    //터치 시작 점
-                   PREV_X = mParams.x;                            //뷰의 시작 점
-                   PREV_Y = mParams.y;                            //뷰의 시작 점
-                   break;
-               case MotionEvent.ACTION_MOVE:
-                   int x = (int)(motionEvent.getRawX() - START_X);    //이동한 거리
-                   int y = (int)(motionEvent.getRawY() - START_Y);    //이동한 거리
-
-                   //터치해서 이동한 만큼 이동 시킨다
-                   mParams.x = PREV_X + x;
-                   mParams.y = PREV_Y + y;
-
-                   optimizePosition();        //뷰의 위치 최적화
-                   wm.updateViewLayout(mView, mParams);    //뷰 업데이트
-                   break;
-           }
-           return true;
-       }
-   };
 
     private void setMaxPosition() {
         DisplayMetrics matrix = new DisplayMetrics();
         wm.getDefaultDisplay().getMetrics(matrix);		//화면 정보를 가져와서
 
-        MAX_X = matrix.widthPixels - mView.getWidth();			//x 최대값 설정
-        MAX_Y = matrix.heightPixels - mView.getHeight();			//y 최대값 설정
+        MAX_X = matrix.widthPixels - fstView.getWidth();			//x 최대값 설정
+        MAX_Y = matrix.heightPixels - fstView.getHeight();			//y 최대값 설정
     }
 
     private void optimizePosition() {
@@ -124,11 +176,17 @@ public class MyService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+
         if(wm != null) {
-            if(mView != null) {
-                wm.removeView(mView);
-                mView = null;
+            if(fstView != null) {
+                wm.removeView(fstView);
+                fstView = null;
             }
+            else if(scdView != null){
+                wm.removeView(scdView);
+                scdView = null;
+            }
+            Log.d("myLog", "myService onDestroy()");
             wm = null;
         }
     }
